1961f1646e6251c49061a772d97f48c6362508ec
Nahuel Garcia <90420052+NahuelGarcia024@users.noreply.github.com>
2023-11-10 00:06:53 -0300

    Add files via upload

---
+++ src/TodoCounter/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './TodoCounter.css';
+
+function TodoCounter({totalTodos, completedTodos}) {
+  
+  
+  return (
+    <h2 className="TodoCounter">Has completado {completedTodos} de {totalTodos} TODOs</h2>
+  );
+}
+
+export { TodoCounter };
